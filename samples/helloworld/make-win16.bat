a6c8d04dbac7957423dbaf2dd9f998af28dbc8ff
Stefano Moioli <smxdev4@gmail.com>
2021-10-24 16:58:01 +0200

    Moved sample to samples/helloworld

--- samples/helloworld/make-win16.bat
+++ samples/helloworld/make-win16.bat
@@ -5,7 +5,8 @@ cmake .. -G"MinGW Makefiles" ^
 	-DCL_ROOT="H:/msvc20/MSVC15" ^
 	-DMSVC_VER=1 ^
 	-DCL_TARGET_OS=WIN16 ^
-	-DCL_QUICKWIN=OFF ^
+	-DCL_QUICKWIN=ON ^
+	-DCL_MMODEL=SMALL ^ 
 	-DCMAKE_TOOLCHAIN_FILE=..\..\cmake\cl.cmake &^
 mingw32-make clean &^
 mingw32-make
